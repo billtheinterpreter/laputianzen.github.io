a8b02fb3c1851df35d7ebe1ea80e3bd68d498d28
Peter Tsai <laputianzen@gmail.com>
2014-01-15 22:41:47 +0800

    entirely change set Markers code and define new field in test data

--- load_test_map_tag.js
+++ load_test_map_tag.js
@@ -4,12 +4,14 @@
 }*/
 
 var mapLocationArray = [];
+var red_icon = "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
+var yellow_icon = "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png";
 
-mapLocationArray.push({id:0, latitude:25.014908, longitude:121.534216, title:'捷運公館站'});
-mapLocationArray.push({id:1, latitude:25.026155, longitude:121.527378, title:'師大校本部'});
-mapLocationArray.push({id:2, latitude:25.012305, longitude:121.533301, title:'自來水園區'});
-mapLocationArray.push({id:3, latitude:25.013345, longitude:121.543075, title:'台灣科技大學'});
-mapLocationArray.push({id:4, latitude:25.024553, longitude:121.545211, title:'國北師'});
+mapLocationArray.push({id:0, latitude:25.014908, longitude:121.534216, title:'捷運公館站', icon:yellow_icon});
+mapLocationArray.push({id:1, latitude:25.026155, longitude:121.527378, title:'師大校本部', icon:red_icon});
+mapLocationArray.push({id:2, latitude:25.012305, longitude:121.533301, title:'自來水園區',icon:red_icon});
+mapLocationArray.push({id:3, latitude:25.013345, longitude:121.543075, title:'台灣科技大學', icon:red_icon});
+mapLocationArray.push({id:4, latitude:25.024553, longitude:121.545211, title:'國北師', icon:red_icon});
 
 mapLocationArray[0].contentString = '<div id="content">'+
       '<div id="siteNotice">'+
@@ -25,10 +27,8 @@ mapLocationArray[0].contentString = '<div id="content">'+
 mapLocationArray[1].contentString = '<div id="content">'+
       '<div id="siteNotice">'+
       '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Gongguan</h1>'+
-      '<div id="bodyContent">'+ '<p> 簡單的公館商圈介紹 </p>' +    
-      '<img src="http://www.lanfox.com.tw/uploads/13124292386448zz.jpg" height="150" width="200">'+ '<br>' +
-      "<iframe width='250' height='167' src='http://www.youtube.com/embed/eLRlHcCiNjg' frameborder='0' ></iframe>" +
+      '<h1 id="firstHeading" class="firstHeading">NTNU</h1>'+
+      '<div id="bodyContent">'+ '<p> 徵求師大本部介紹 </p>' +    
       '</div>'+
       // chat room link
       '<a href = firetube.html> 進入聊天室 </a>' +
@@ -36,10 +36,8 @@ mapLocationArray[1].contentString = '<div id="content">'+
 mapLocationArray[2].contentString = '<div id="content">'+
       '<div id="siteNotice">'+
       '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Gongguan</h1>'+
-      '<div id="bodyContent">'+ '<p> 簡單的公館商圈介紹 </p>' +    
-      '<img src="http://www.lanfox.com.tw/uploads/13124292386448zz.jpg" height="150" width="200">'+ '<br>' +
-      "<iframe width='250' height='167' src='http://www.youtube.com/embed/eLRlHcCiNjg' frameborder='0' ></iframe>" +
+      '<h1 id="firstHeading" class="firstHeading">Water Supply Center</h1>'+
+      '<div id="bodyContent">'+ '<p> 徵求自來水園區介紹 </p>' +    
       '</div>'+
       // chat room link
       '<a href = firetube.html> 進入聊天室 </a>' +
@@ -47,10 +45,8 @@ mapLocationArray[2].contentString = '<div id="content">'+
 mapLocationArray[3].contentString = '<div id="content">'+
       '<div id="siteNotice">'+
       '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Gongguan</h1>'+
-      '<div id="bodyContent">'+ '<p> 簡單的公館商圈介紹 </p>' +    
-      '<img src="http://www.lanfox.com.tw/uploads/13124292386448zz.jpg" height="150" width="200">'+ '<br>' +
-      "<iframe width='250' height='167' src='http://www.youtube.com/embed/eLRlHcCiNjg' frameborder='0' ></iframe>" +
+      '<h1 id="firstHeading" class="firstHeading">NTUST</h1>'+
+      '<div id="bodyContent">'+ '<p> 徵求介紹 </p>' +    
       '</div>'+
       // chat room link
       '<a href = firetube.html> 進入聊天室 </a>' +
@@ -58,14 +54,22 @@ mapLocationArray[3].contentString = '<div id="content">'+
 mapLocationArray[4].contentString = '<div id="content">'+
       '<div id="siteNotice">'+
       '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Gongguan</h1>'+
-      '<div id="bodyContent">'+ '<p> 簡單的公館商圈介紹 </p>' +    
-      '<img src="http://www.lanfox.com.tw/uploads/13124292386448zz.jpg" height="150" width="200">'+ '<br>' +
-      "<iframe width='250' height='167' src='http://www.youtube.com/embed/eLRlHcCiNjg' frameborder='0' ></iframe>" +
+      '<h1 id="firstHeading" class="firstHeading">NTUE</h1>'+
+      '<div id="bodyContent">'+ '<p> 徵求簡單介紹 </p>' +    
       '</div>'+
       // chat room link
       '<a href = firetube.html> 進入聊天室 </a>' +
       '</div>';      
+
+default_content = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading"></h1>'+
+      '<div id="bodyContent">'+ '<p></p>' +    
+      '</div>'+
+      // chat room link
+      '<input type="button" value="建立聊天室">' +
+      '</div>';
 
 /*console.log("geo_latidue:" + mapLocationArray[0].latitude +"geo_longitude:" + mapLocationArray[0].longitude);
 console.log(mapLocationArray[0].id);
